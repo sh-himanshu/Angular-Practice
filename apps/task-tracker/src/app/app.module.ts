c48c1c68c14223949490d65be2f3ec2ccbc6e6bd
sh-himanshu <88159798+sh-himanshu@users.noreply.github.com>
2022-09-19 11:32:40 +0530

    :sparkles: feat(task-tracker): Create header component

--- apps/task-tracker/src/app/app.module.ts
+++ apps/task-tracker/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './components/button/button.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
--- apps/task-tracker/tailwind.config.js
+++ apps/task-tracker/tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
